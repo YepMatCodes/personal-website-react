8cee15f1a6f340d663e64ddebd99ab2ab0dd2411
Mathew Norman <spam@matnorman.com>
2024-04-24 11:37:08 +1000

    Added mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import './MobileApp.css'
 import Home from './pages/Home'
+import MobileHome from './pages/MobileHome'
 import './css/icons.css'
+import { isMobile } from 'react-device-detect';
 
 function App() {
-  return (
-    <BrowserRouter>
+  if (isMobile) {
+    return (
+      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
+        <Route path="/" element={<MobileHome />}>
+          <Route index element={<MobileHome />} />
           {/* <Route path="*" element={<NoPage />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+    );
+  } else {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+            {/* <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
