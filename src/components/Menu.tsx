954fe835a8cd4dc3f827909fb93f6aac12a478cb
Mathew Norman <spam@matnorman.com>
2024-04-28 19:12:33 +1000

    Fixed naming

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,12 +4,12 @@ function Menu() {
     return (
       <div className="external-links-menu">
         <div className="menu-icon-container">
-          <button className="menu-icon-button">
-              <i className="lni lni-github-original"></i>
-          </button>
-          <button className="menu-icon-button">
-              <i className="lni lni-linkedin-original"></i>
-          </button>
+        <button className="menu-icon-button">
+            <i className="lni lni-github-original"></i>
+        </button>
+        <button className="menu-icon-button">
+            <i className="lni lni-linkedin-original"></i>
+        </button>
           <button className="menu-icon-button">
               <i className="lni lni-linkedin-original"></i>
           </button>
