b9b05b4af87c2a42cf67ab8b2417f1886e509230
Mathew Norman <spam@matnorman.com>
2024-04-28 20:37:29 +1000

    Refactored logo loading (mobile and desktop)

---
+++ src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import { isMobile } from 'react-device-detect';
+
+import { ReactComponent as DesktopLogo } from "../images/logo.svg";
+import { ReactComponent as MobileLogo } from "../images/logo-mobile.svg";
+
+function Logo() {
+    if (isMobile) {
+        return ( <MobileLogo /> );
+    } else {
+        return ( <DesktopLogo /> );
+    }
+}
+
+export default Logo;
